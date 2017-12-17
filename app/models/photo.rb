bdccb9ac4938b2d326b5b3fc1ff8a9f838d29961
Alex Berneau <haldir.alex@gmail.com>
2017-12-17 03:16:59 +0100

    places index

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,14 @@
 class Photo < ApplicationRecord
   belongs_to :place
 
-  has_attached_file :image, :path => ":rails_root/public/images/:id/:filename", :url  => "/images/:id/:filename", styles: { medium: "300x300>", thumb: "100x100>" }
+  has_attached_file :image,
+    :path => ":rails_root/public/images/:id/:filename",
+    :url  => "/images/:id/:filename",
+    styles: {
+      :thumb => "100x100#",
+      :medium => "300x300>",
+      :high => "1280x720"
+    }
 
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
